0531156d1be5cd18b979ca6cd1a38ea3f5207518
LorennFdz <loreenfernandez.14@gmail.com>
2024-11-20 19:34:14 -0300

    Se agregaron tests

--- micro-tpe/microservice_administrator/src/test/java/microservice_administrator/TEST_Admin.java
+++ micro-tpe/microservice_administrator/src/test/java/microservice_administrator/TEST_Admin.java
@@ -1,7 +1,9 @@
 package microservice_administrator;
 
-import microservicioAdmin.feignClients.model.TripDTO;
+import microservicioAdmin.dto.BillingDTO;
+import microservicioAdmin.feignClients.AccountFeignClients;
 import microservicioAdmin.services.AdminService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +13,27 @@ public class TEST_Admin {
 
   @Autowired
   private AdminService adminService;
+  private AccountFeignClients accountFeignClients;
 
   @Test
-  public void TEST_calculateCost(){
-    TripDTO tripDTO = new TripDTO();
+  public void test_calculateRateOfTrip_tripNull() {
+    Double kmTraveled = 10.0;
+    try {
+      BillingDTO result = adminService.calculateRateOfTrip(null, kmTraveled);
+      Assert.assertNull(result);
+    } catch (Exception e) {
+      Assert.fail("Fallo el test: " + e.getMessage());
+    }
+  }
 
+  @Test
+  public void test_annulledAccount_accountNotFound() {
+    boolean annul = true;
+
+    Exception exception = Assert.assertThrows(Exception.class, () -> {
+      accountFeignClients.annulledAccount(-1L, annul);
+    });
+
+    Assert.assertEquals("La cuenta no existe", exception.getMessage());
   }
 }
